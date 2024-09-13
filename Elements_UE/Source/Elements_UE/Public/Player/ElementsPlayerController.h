f2c5f5da4ae98cb269b6032bd2d10f183e93df81
Max <mharris382@gmail.com>
2024-09-13 14:41:13 -0400

    created PlayerState and PlayerController classes
    added Gameplay tag Effect.RemoveOnDeath

---
+++ Elements_UE/Source/Elements_UE/Public/Player/ElementsPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ElementsPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ELEMENTS_UE_API AElementsPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
