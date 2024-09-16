f00e706198e4bc90e37108caa5952b09e5566625
Max <mharris382@gmail.com>
2024-09-16 00:32:56 -0400

    actually made a cast ability woot

--- Elements_UE/Intermediate/Build/Win64/UnrealEditor/Inc/Elements_UE/UHT/Elements_UE.init.gen.cpp
+++ Elements_UE/Intermediate/Build/Win64/UnrealEditor/Inc/Elements_UE/UHT/Elements_UE.init.gen.cpp
@@ -27,13 +27,13 @@ void EmptyLinkFunctionForGeneratedCodeElements_UE_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x97D8B47D,
-				0x72E43FB1,
+				0x239304E3,
+				0xC8869B81,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Elements_UE.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_Elements_UE.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Elements_UE(Z_Construct_UPackage__Script_Elements_UE, TEXT("/Script/Elements_UE"), Z_Registration_Info_UPackage__Script_Elements_UE, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x97D8B47D, 0x72E43FB1));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Elements_UE(Z_Construct_UPackage__Script_Elements_UE, TEXT("/Script/Elements_UE"), Z_Registration_Info_UPackage__Script_Elements_UE, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x239304E3, 0xC8869B81));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
